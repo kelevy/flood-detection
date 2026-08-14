12bb152b2e704fd2170601603dd5448022b0f985
Kevin Levy <klevy@Mac.lan>
2026-08-14 20:35:40 +0200

    Pin safetensors<0.4 for Python 3.7 compatibility

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="flood_detection_trainer",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        "torch",
-        "torchvision",
-        "segmentation-models-pytorch",
-        "rasterio",
-        "numpy",
-        "tqdm",
-    ],
-)
+install_requires=[
+    "torch",
+    "torchvision",
+    "segmentation-models-pytorch",
+    "rasterio",
+    "numpy",
+    "tqdm",
+    "safetensors<0.4",
+],
